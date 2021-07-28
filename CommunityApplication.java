4b0892ea9617c5c70fcf08c0a09a4cc9e94369a7
huyuhan002 <huyuhan002@ke.com>
2021-07-28 17:50:04 +0800

    second

---
+++ CommunityApplication.java
@@ -0,0 +1,22 @@
+package life.majiang.community;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@MapperScan("life.majiang.community.mapper")
+@EnableScheduling
+public class CommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunityApplication.class, args);
+		System.out.print("fail");
+        System.out.print("sussgess");
+		System.out.print("sussgess01-demo");
+		System.out.print("sussgess02-demo");
+		System.out.print("sussgess03-demo");
+    }
+
+}
---
+++ readme.txt
@@ -0,0 +1,9 @@
+this is my first commit
+this is my second commit
+this is my third commit
+在github远程修改
+在本机修改-1431
+在github远程第二次修改
+在本机第二次修改-1458
+在github远程第三次修改
+
